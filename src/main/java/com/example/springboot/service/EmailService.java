c57f1c1153af7cbd7ca42374039434c0cceaaeeb
Graham G <grahamg@users.noreply.github.com>
2021-07-22 03:16:06 -0500

    Update EmailService.java

--- src/main/java/com/example/springboot/service/EmailService.java
+++ src/main/java/com/example/springboot/service/EmailService.java
@@ -1,3 +1,25 @@
 package com.example.springboot.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
+public class EmailService {
+	
+	 @Value("${email.username}")
+	 private String username;
+	 
+	 @Value("${email.pwd}")
+	 private String pwd;
+
+	 @Autowired
+	 private EmailConfig emailConfig;
+	 
+	public void sendEmail(){
+    System.out.println("reading value from propertes file using @value annotation");
+    System.out.println("username ="+ username);
+    System.out.println("pwd ="+ pwd);
+	}
+	
+}
